7ea52b28fb7378f2d065c078fe500c9091839ab4
nelson.ferro <nelson.ferro@SCBGLP00133.SiasColombia.local>
2020-11-20 15:12:36 -0500

    correcion conexion

--- src/modelo/ConsultaDAO.java
+++ src/modelo/ConsultaDAO.java
@@ -4,11 +4,101 @@
  * and open the template in the editor.
  */
 package modelo;
+import java.sql.*;
+import java.util.ArrayList;
 
 /**
  *
- * @author ASUS
+ * @author wilso
  */
 public class ConsultaDAO {
+    Conexion conexion;
     
+    
+    public ConsultaDAO(){
+        conexion= new Conexion();
+        
+    }
+    
+    public String insertconsulta(String id,String id_historia_clinica,String id_veterinario, Date fecha ,String id_tipo_consulta,String diagnostico ){
+        String rptaRegistro=null;
+        try {
+            Connection accesoDB = conexion.getConexion();
+             String SQL_INSERT = "INSERT INTO consulta (id_historia_clinica, id_veterinario, fecha,id_tipo_consulta,diagnostico) VALUES (?,?,?,?,?)";
+             PreparedStatement ps = accesoDB.prepareStatement(SQL_INSERT);
+            ps.setString(1, id_historia_clinica);
+            ps.setString(2, id_veterinario);
+            ps.setDate(3, fecha);
+            ps.setString(4, id_tipo_consulta);
+            ps.setString(5, diagnostico);
+            int numAfectadas = ps.executeUpdate();
+            if(numAfectadas>0){
+                rptaRegistro="registro exitoso";
+            }
+        } catch (Exception e){
+        }
+        return rptaRegistro;
+    }
+    
+    public String editarconsulta(String id,String id_historia_clinica,String id_veterinario, Date fecha ,String id_tipo_consulta,String diagnostico){
+        String rptaRegistro=null;
+        try {
+            Connection accesoDB = conexion.getConexion();
+             String SQL_INSERT = "update consulta set id_historia_clinica=?, id_veterinario=?, fecha=?, id_tipo_consulta=?, diagnostico=? where id_veterinario=?";
+             PreparedStatement ps = accesoDB.prepareStatement(SQL_INSERT);
+            ps.setString(1, id_historia_clinica);
+            ps.setString(2, id_veterinario);
+            ps.setDate(3, fecha);
+            ps.setString(4, id_tipo_consulta);
+            ps.setString(5, diagnostico);
+            ps.setInt(7, Integer.parseInt(id));
+            int numAfectadas = ps.executeUpdate();
+            if(numAfectadas>0){
+                rptaRegistro="editado exitosamente";
+            }
+        } catch (Exception e){
+        }
+        return rptaRegistro;
+    }
+    
+     public String eliminarconsulta(String id){
+        String rptaRegistro=null;
+        try {
+            Connection accesoDB = conexion.getConexion();
+             String SQL_INSERT = "delete from consulta where id_consulta=?";
+             PreparedStatement ps = accesoDB.prepareStatement(SQL_INSERT);
+             ps.setInt(1, Integer.parseInt(id));
+            int numAfectadas = ps.executeUpdate();
+            if(numAfectadas>0){
+                rptaRegistro="eliminado exitosamente";
+            }
+        } catch (Exception e){
+        }
+        return rptaRegistro;
+    }
+       
+    public ArrayList<Veterinario> listveterinario(){
+        ArrayList listaconsulta = new ArrayList();
+         Consulta consulta;
+         try {
+            Connection acceDB = conexion.getConexion();
+            PreparedStatement ps = acceDB.prepareStatement("select * from consulta"); 
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                consulta = new Consulta() ;
+                consulta.setId(rs.getString (1));
+                consulta .setid_historia_clinica (rs.getString (2));
+                consulta.setid_veterinario (rs.getString (3));
+                consulta.setfecha (rs.getDate (4));
+                consulta.setid_tipo_consulta (rs.getString (5));
+                consulta.setdiagnostico(rs.getString (6));
+               listaconsulta.add(consulta);
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+           
+        return listaconsulta; 
+    }
 }
--- src/vista/JFConsulta.java
+++ src/vista/JFConsulta.java
@@ -5,6 +5,8 @@
  */
 package vista;
 
+import controlador.ControladorConsulta;
+import modelo.ConsultaDAO;
 /**
  *
  * @author ASUS
@@ -27,23 +29,211 @@ public class JFConsulta extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jCalendar2 = new com.toedter.calendar.JCalendar();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        txId = new javax.swing.JTextField();
+        txid_historia_clinica = new javax.swing.JTextField();
+        txid_veterinario = new javax.swing.JTextField();
+        jCalendar1 = new com.toedter.calendar.JCalendar();
+        txid_tipo_consulta = new javax.swing.JTextField();
+        txdiagnostico = new javax.swing.JTextField();
+        btnRegistrar = new javax.swing.JButton();
+        btnEliminar = new javax.swing.JButton();
+        btnGuardar = new javax.swing.JButton();
+        btnListar = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        listConsulta = new javax.swing.JTable();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("id");
+
+        jLabel2.setText("id_historia_clinica");
+
+        jLabel3.setText("id_veterinario");
+
+        jLabel4.setText("fecha");
+
+        jLabel5.setText("id_tipo_consulta");
+
+        jLabel6.setText("diagnostico");
+
+        btnRegistrar.setText("Registrar");
+        btnRegistrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegistrarActionPerformed(evt);
+            }
+        });
+
+        btnEliminar.setText("Eliminar");
+        btnEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEliminarActionPerformed(evt);
+            }
+        });
+
+        btnGuardar.setText("Editar");
+        btnGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGuardarActionPerformed(evt);
+            }
+        });
+
+        btnListar.setText("Listar");
+        btnListar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnListarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel4))
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                            .addComponent(jLabel5)
+                            .addGap(17, 17, 17)))
+                    .addComponent(jLabel6))
+                .addGap(45, 45, 45)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(txid_historia_clinica)
+                    .addComponent(txId)
+                    .addComponent(txid_veterinario)
+                    .addComponent(jCalendar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(txid_tipo_consulta)
+                    .addComponent(txdiagnostico))
+                .addGap(97, 97, 97)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btnRegistrar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnEliminar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnGuardar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnListar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(129, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(txId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnRegistrar))
+                .addGap(24, 24, 24)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(txid_historia_clinica, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnEliminar))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(26, 26, 26)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(txid_veterinario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(41, 41, 41)
+                        .addComponent(btnGuardar)))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(22, 22, 22)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel4)
+                            .addComponent(jCalendar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(33, 33, 33)
+                        .addComponent(btnListar)))
+                .addGap(41, 41, 41)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txid_tipo_consulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(txdiagnostico, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(53, Short.MAX_VALUE))
+        );
+
+        listConsulta.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(listConsulta);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 602, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(73, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(83, 83, 83))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
+    private void btnRegistrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarActionPerformed
+        // TODO add your handling code here:
+              ConsultaDAO modeloCRUD = new ConsultaDAO();
+     ControladorConsulta control=new ControladorConsulta(this,modeloCRUD);
+     control.guardarConsulta(evt);
+        
+    }//GEN-LAST:event_btnRegistrarActionPerformed
+
+    private void btnEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEliminarActionPerformed
+        // TODO add your handling code here:
+        ConsultaDAO modeloCRUD = new ConsultaDAO();
+     ControladorConsulta control=new ControladorConsulta(this,modeloCRUD);
+     control.eliminarConsulta();
+    }//GEN-LAST:event_btnEliminarActionPerformed
+
+    private void btnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarActionPerformed
+        // TODO add your handling code here:
+          ConsultaDAO modeloCRUD = new ConsultaDAO();
+     ControladorConsulta control=new ControladorConsulta(this,modeloCRUD);
+     control.editarConsulta();
+    }//GEN-LAST:event_btnGuardarActionPerformed
+
+    private void btnListarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnListarActionPerformed
+        // TODO add your handling code here:
+         ConsultaDAO modeloCRUD = new ConsultaDAO();
+     ControladorConsulta control=new ControladorConsulta(this,modeloCRUD);
+     control.llenarTabla(listConsulta);
+    }//GEN-LAST:event_btnListarActionPerformed
+
+   /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
@@ -60,24 +250,49 @@ public class JFConsulta extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(JFConsulta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JFVeterinario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(JFConsulta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JFVeterinario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(JFConsulta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JFVeterinario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(JFConsulta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JFVeterinario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new JFConsulta().setVisible(true);
+                new JFVeterinario().setVisible(true);
+                
+               
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JButton btnEliminar;
+    public javax.swing.JButton btnGuardar;
+    public javax.swing.JButton btnListar;
+    public javax.swing.JButton btnRegistrar;
+    private com.toedter.calendar.JCalendar jCalendar1;
+    private com.toedter.calendar.JCalendar jCalendar2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    public javax.swing.JTable listConsulta;
+    public javax.swing.JTextField txId;
+    public javax.swing.JTextField txdiagnostico;
+    public javax.swing.JTextField txid_historia_clinica;
+    public javax.swing.JTextField txid_tipo_consulta;
+    public javax.swing.JTextField txid_veterinario;
     // End of variables declaration//GEN-END:variables
 }
